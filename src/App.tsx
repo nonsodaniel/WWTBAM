c466da9b433ccec846e30a210661ab0ff9f69ff3
nonsodaniel <nonsodaniel07@gmail.com>
2023-08-03 00:04:17 +0100

    Created layout for question options

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { moneyList } from "./utils/db";
+
 function App() {
-  return <div className="app">Hello world</div>;
+  const [questionNumber, setQuestionNumber] = useState(1);
+
+  return (
+    <div className="app">
+      <div className="container">Main</div>
+      <div className="pyramid-container">
+        <div className="money-container">
+          <ul className="money-list">
+            {moneyList.map((money, index) => (
+              <li
+                key={money.id}
+                className={`money-item ${
+                  questionNumber === money.id ? "active" : ""
+                }`}
+              >
+                <span className="number">{money.id}</span>
+                <span className="amount">$ {money.amount}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/utils/db.ts
@@ -0,0 +1,10 @@
+export const moneyList = [
+  { id: 1, amount: 100 },
+  { id: 2, amount: 200 },
+  { id: 3, amount: 300 },
+  { id: 4, amount: 400 },
+  { id: 8, amount: 800 },
+  { id: 12, amount: 1200 },
+  { id: 24, amount: 24000 },
+  { id: 50, amount: 50000 },
+];
